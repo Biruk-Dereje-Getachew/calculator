e0448ff7e64684a0f2fbc245eb8b32d2af0033a6
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-03-25 20:34:32 +0300

    Allow same repeated operations and clear all input

--- js/script.js
+++ js/script.js
@@ -15,38 +15,65 @@ function divide(num1, num2) {
 }
 
 let operand1, operator, operand2;
+let clearOnTextInput = false;
 
 function operate(num1, num2, operator) {
   switch (operator) {
     case "+":
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case "-":
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case "*":
-      multiply(num1, num2);
-      break;
+      return multiply(num1, num2);
     case "/":
-      divide(num1, num2);
-      break;
+      return divide(num1, num2);
   }
 }
 
 const display = document.querySelector(".display");
 const buttons = document.querySelector(".buttons");
 
-buttons.addEventListener("click", (evt) => {
-  display.textContent = evt.target.classList.contains("operator")
-    ? ""
-    : display.textContent + evt.target.textContent;
-});
+buttons.addEventListener(
+  "click",
+  (evt) => {
+    if (clearOnTextInput) {
+      display.textContent = "";
+      clearOnTextInput = false;
+    }
+
+    display.textContent += evt.target.classList.contains("operator")
+      ? ""
+      : evt.target.textContent;
+  },
+  true,
+);
 
 const operators = document.querySelectorAll(".operator");
+
 operators.forEach((operatorBtn) => {
   operatorBtn.addEventListener("click", (evt) => {
-    operand1 = Number(display.textContent);
     operator = evt.target.textContent;
-    display.textContent = "";
+    if (!operand1 && !operand2) {
+      operand1 = Number(display.textContent);
+      display.textContent = "";
+    } else if (!operand2 && operand1) {
+      operand2 = Number(display.textContent);
+      operand1 = operate(operand1, operand2, operator);
+      display.textContent = operand1;
+      clearOnTextInput = true;
+      operand2 = null;
+      debugger;
+    }
   });
+});
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", (evt) => {
+  // Clear display
+  display.textContent = "";
+  // Update variables
+  operand1 = null;
+  operator = null;
+  operand2 = null;
+  clearOnTextInput = false;
 });
